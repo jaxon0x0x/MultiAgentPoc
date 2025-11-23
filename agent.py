b26b5183746f44b5e91f95cd3cdfc5eb64d6ec18
janek <wierzbowiczjan@gmail.com>
2025-11-23 21:24:46 +0100

    added function callin

--- agent.py
+++ agent.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 import os
 from api import AssistantFnc
 load_dotenv()
-from prompts import WELCOME_MESSAGE, INSTRUCTIONS
+from prompts import WELCOME_MESSAGE, INSTRUCTIONS, LOOKUP_VIN_MESSAGE
 
 
 async def entrypoint(ctx: JobContext):
@@ -39,6 +39,33 @@ async def entrypoint(ctx: JobContext):
     )
     session.response.create()
 
+    @session.on("user_speach_commited")
+    def on_user_speach_commited(msg: llm.ChatMessage):
+        if isinstance(msg.content, list):
+            msg.content = "\n".join("[image]" if isinstance(x, llm.ChatImage) else x for x in msg)
+
+        if assistant_fnc.has_car():
+            handle_query(msg)
+        else:
+            find_profile(msg)
+
+        def find_profile(msg: llm.ChatMessage):
+            session.conversation.item.create(
+                llm.ChatMessage(
+                    role="system",
+                    content=LOOKUP_VIN_MESSAGE(msg)
+                )
+            )
+            session.response.create()
+
+        def handle_query(msg: llm.ChatMessage):
+            session.conversation.item.create(
+                llm.ChatMessage(
+                    role="user",
+                    content=msg.content
+                )
+            )
+            session.response.create()
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
 
